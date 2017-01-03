d0894d4d7cfecb738e8c2384b2605e2b6b331f82
David Ivy <davidivy003@gmail.com>
2017-01-03 17:56:17 -0500

    Refactor deck component to use function based view

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-class Deck extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Hello World React JS, Redux, and create-react-app</h2>
-        </div>
-        <p className="App-intro">
-          Hello, {this.props.deckId}
-          {' '}
-        </p>
-      </div>
-    );
-  }
-}
+const Deck = props => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Hello World React JS, Redux, and create-react-app</h2>
+    </div>
+    <p className="App-intro">
+      Hello, {props.deckId}
+      {' '}
+    </p>
+  </div>
+)
 
 const mapStateToProps = (state) => {
   console.log('map to state to props, state.deckId: ', state);
