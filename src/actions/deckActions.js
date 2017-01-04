5c6d3215220c3b9a76f44842724e7fcd4bc1e9e0
David Ivy <davidivy003@gmail.com>
2017-01-04 18:08:42 -0500

    Add game actions

--- src/actions/deckActions.js
+++ src/actions/deckActions.js
@@ -8,12 +8,25 @@ const drawCardAction = (card) => ({
   card: card
 })
 
-export const drawCard = (deckId) => {
-  console.log('deckid', deckId)
+const guessAction = (guess, newCard, lastCard) => ({
+  type: 'GUESS',
+  guess: guess,
+  newCard: newCard,
+  lastCard: lastCard
+})
+
+export const drawCard = (deckId, guess = 0, lastCard) => {
+  console.log('drawCard', deckId, guess)
   return function (dispatch) {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
       .then(response => response.json())
-      .then(data => dispatch(drawCardAction(data.cards[0])))
+      .then(data => {
+        let card = data.cards[0]
+        dispatch(drawCardAction(card))
+        if (guess !== 0) {
+          dispatch(guessAction(guess, card.value, lastCard))
+        }
+      })
   }
 }
 
@@ -28,3 +41,6 @@ export const createDeck = () => {
       })
   }
 }
+
+export const guessHigher = (deckId, card) => drawCard(deckId, 1, card)
+export const guessLower = (deckId, card) => drawCard(deckId, -1, card)
